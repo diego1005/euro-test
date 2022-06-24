8160019cc31fa289b14ed700ca60ac0fb6f63eff
diego1005 <dmaguilar82@gmail.com>
2022-06-24 17:54:08 -0300

    First look to cuts structure

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, '../../database/mov_list.json');
+const filePath = path.join(__dirname, '../../database/mov2_list.json');
 let movList = fs.readFileSync(filePath, "utf-8");
 movList = JSON.parse(movList);
 
 const productsController = {
     detail: (req, res) => {
-        let tela = req.body.cloth;
-        let prodList = movList.filter(el => el.tela == tela);
+        let cod_tela = req.body.cloth;
+        let prodList = movList.filter(el => el.cod_tela == cod_tela);
         let title = prodList[0].descripcion;
         let total = totalizar(prodList);
         res.render("detail", { title: title, prod: prodList, total: total, edit: false });
@@ -20,7 +20,7 @@ const productsController = {
         new_prod.id = movList[len-1].id + 1;
         movList.push(new_prod);
         movList = movList.sort((a, b) => Date.parse(a.fecha) - Date.parse(b.fecha));
-        let prodList = movList.filter(el => el.tela == new_prod.tela);
+        let prodList = movList.filter(el => el.cod_tela == new_prod.cod_tela);
         let total = totalizar(prodList);
         let write_movList = JSON.stringify(movList);
         fs.writeFileSync(filePath, write_movList);
@@ -29,7 +29,7 @@ const productsController = {
     edit: (req, res) => {
         let prodEdit = movList.find(el => el.id == req.params.id);
         let title = prodEdit.descripcion;
-        let prodList = movList.filter(el => el.tela == prodEdit.tela);
+        let prodList = movList.filter(el => el.cod_tela == prodEdit.cod_tela);
         let total = totalizar(prodList);
         res.render("detail", { title: title, prod: prodList, prodE: prodEdit, total: total, edit: true });
     },
@@ -39,7 +39,7 @@ const productsController = {
         movList.push(prodEdit);
         let title = prodEdit.descripcion;
         movList = movList.sort((a, b) => Date.parse(a.fecha) - Date.parse(b.fecha));
-        let prodList = movList.filter(el => el.tela == prodEdit.tela);
+        let prodList = movList.filter(el => el.cod_tela == prodEdit.cod_tela);
         let total = totalizar(prodList);
         let write_movList = JSON.stringify(movList);
         fs.writeFileSync(filePath, write_movList);
@@ -50,7 +50,7 @@ const productsController = {
         let title = prodDel.descripcion;
         movList = movList.filter(e => e.id != req.params.id);
         movList = movList.sort((a, b) => Date.parse(a.fecha) - Date.parse(b.fecha));
-        let prodList = movList.filter(el => el.tela == prodDel.tela);
+        let prodList = movList.filter(el => el.cod_tela == prodDel.cod_tela);
         let total = totalizar(prodList);
         let write_movList = JSON.stringify(movList);
         fs.writeFileSync(filePath, write_movList);
--- test.js
+++ test.js
@@ -1,14 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, '/database/lists/cloth_list.json');
+const filePath = path.join(__dirname, '/database/mov2_list.json');
 let prod = fs.readFileSync(filePath, "utf-8");
 prod = JSON.parse(prod);
 
 
-prod= prod.map((el, idx) => el = {id: idx+1, ...el});
-prod = JSON.stringify(prod);
-fs.writeFileSync(path.join(__dirname, '/database/lists/cloths_list.json'), prod);
+let recs = prod.map(el =>  {
+    el.ancho = (el.tipo === "EGRESO") ? (parseFloat(el.ancho) - parseFloat(el.rollo)).toFixed(2) : el.ancho;
+    delete el.tipo;
+    delete el.prov_cli;
+    delete el.tela;
+    delete el.rollo;
+    delete el.color
+
+    return el;
+})
+
+console.log(recs);
+
+// prod= prod.map((el, idx) => el = {id: idx+1, ...el});
+// prod = JSON.stringify(prod);
+// fs.writeFileSync(path.join(__dirname, '/database/mov3_list.json'), prod);
 
 
 
